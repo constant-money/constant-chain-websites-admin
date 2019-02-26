065d41302004bb0c190801bd3a213fb129583c2c
hieuqautonomous <hieu.q@autonomous.nyc>
2019-02-26 12:22:41 +0700

    check auth for admin

--- app/Controllers/Http/Admin/AuthController.js
+++ app/Controllers/Http/Admin/AuthController.js
@@ -3,15 +3,23 @@
 const Hash = use('Hash')
 class AuthController {
     async login({ request, response, auth, view }) {
-        if (auth.user != null) {
+        try {
+            await auth.check()
             return response.route('Admin/HomeController.dashboard')
+        } catch (error) {
         }
         if (request.method() == 'POST') {
             try {
                 const { email, password } = request.all()
-                let authCheck = await auth.attempt(email, password)
+                let authCheck = await auth
+                    .remember(true)
+                    .attempt(email, password)
                 if (authCheck) {
-                    return response.route('Admin/HomeController.dashboard')
+                    if (auth.user.is_admin != undefined && auth.user.is_admin == true) {
+                        return response.route('Admin/HomeController.dashboard')
+                    } else {
+                        await auth.logout()
+                    }
                 }
             } catch (err) {
                 return view.render('admin/auth/login', {
@@ -22,8 +30,10 @@ class AuthController {
         return view.render('admin/auth/login')
     }
     async logout({ response, auth }) {
-        if (auth.user != null) {
+        try {
+            await auth.check()
             await auth.logout()
+        } catch (error) {
         }
         return response.route('Admin/AuthController.login')
     }
