597da2205d2348c2742332b0f0c0367a002a79ea
Duc Hung <hung.han@notabasement.com>
2019-02-27 16:05:30 +0700

    update provider

--- providers/View/provider.js
+++ providers/View/provider.js
@@ -10,14 +10,6 @@ class ViewProvider extends ServiceProvider {
     const View = this.app.use('Adonis/Src/View')
     View.global('time', () => new Date().getTime())
     View.global('moment', moment)
-    // CREATE LIST PAGE FOR PAGINATION
-    View.global('createPageList', (lastPage=1) => {
-      let list = [];
-      for (let i = 1;i<=lastPage;i++) {
-        list.push(i)
-      }
-      return list;
-    })
   }
 }
 
