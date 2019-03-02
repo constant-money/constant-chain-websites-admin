a2d1b83a9024b7d14835c44b432752fe7e6e7cba
hieuqautonomous <hieu.q@autonomous.nyc>
2019-03-02 10:51:26 +0700

    fix permission

--- app/Middleware/Permission.js
+++ app/Middleware/Permission.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const UserDAO = use('UserDAO')
+// const UserDAO = use('UserDAO')
 
 class Permission {
-    async handle({ request, auth, session, view }, next, ...agrs) {
+    async handle({ request, response, auth, session, view }, next, ...agrs) {
         // try {
         //   await auth.check()
         //   const parameters = JSON.stringify(request.all())
@@ -12,19 +12,29 @@ class Permission {
         // }
         // TODO check user permission for this request
         // users , roles (name), permissions (method, function), role_permissions (role_id, permission_id), user_roles (user_id, role_id)
+        if (agrs[0][0] == 'admin.auth.login' || agrs[0][0] == 'admin.auth.logout') {
+            await next()
+            return
+        }
         let authorized = false
         try {
             await auth.check()
             authorized = true
         } catch (e) {
         }
+        const permissions = session.get('PERMISSIONS')
         if (authorized) {
-            if (! await UserDAO.permission(auth.user.id, request.method().toUpperCase(), agrs[0][0])) {
+            // if (! await UserDAO.permission(auth.user.id, request.method().toUpperCase(), agrs[0][0])) {
+            //     throw new Error('Sorry, your access is refused due to security reasons of our server and also our sensitive data. Please go back to the previous page to continue browsing.');
+            // }
+            if (permissions == null || permissions == undefined) {
+                return response.route('admin.auth.logout')
+            }
+            if (!permissions.includes(`${request.method()}_${agrs[0][0]}`)) {
                 throw new Error('Sorry, your access is refused due to security reasons of our server and also our sensitive data. Please go back to the previous page to continue browsing.');
             }
         }
         if (view && typeof (view.share) === 'function') {
-            const permissions = session.get('PERMISSIONS')
             view.share({
                 permissions: permissions
             })
