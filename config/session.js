7132c4a3f26680ca7be285f8bc7a695891058130
hieuqautonomous <hieu.q@autonomous.nyc>
2019-03-04 09:47:00 +0700

    fix redis

--- config/session.js
+++ config/session.js
@@ -91,5 +91,11 @@ module.exports = {
   | the redis file. But you are free to define an object here too.
   |
   */
-  redis: 'self::redis.local'
+  redis: {
+    host: Env.get('REDIS_HOST', '127.0.0.1'),
+    port: Env.get('REDIS_PORT', 6379),
+    password: Env.get('REDIS_PASSWORD', null),
+    db: 0,
+    keyPrefix: ''
+  }
 }
