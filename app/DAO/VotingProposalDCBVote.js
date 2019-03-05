705148c34496323be53fc18b38d3f895b59d46b8
Duc Hung <hung.han@notabasement.com>
2019-03-05 18:06:12 +0700

    DCB proposal && vote query layer update

--- app/DAO/VotingProposalDCBVote.js
+++ app/DAO/VotingProposalDCBVote.js
@@ -2,6 +2,7 @@
 
 const VotingProposalDCBVoteModel = use('VotingProposalDCBVoteModel')
 
+const moment = require("moment")
 class VotingProposalDCBVote {
     async first(id) {
         return await VotingProposalDCBVoteModel
@@ -13,12 +14,30 @@ class VotingProposalDCBVote {
             .where('id', id).first()
     }
 
-    async find({ votingProposalDCBId, email, page, perPage }) {
+    async find({ votingProposalDCBId, email, page, perPage, vote_amount_from, vote_amount_to, tx_id='', from_date='', to_date='' }) {
         let q = VotingProposalDCBVoteModel
             .query()
             .with('voter')
             .whereNull('deleted_at')
             .where('voting_proposal_dcb_id', '=', votingProposalDCBId)
+
+        if (vote_amount_from) {
+          q = q.whereRaw("vote_amount >= ?", vote_amount_from)
+        }
+        if (vote_amount_to) {
+          q = q.whereRaw("vote_amount <= ? ", vote_amount_to)
+        }
+        if (tx_id) {
+          q = q.where("tx_id", tx_id)
+        }
+        if (from_date) {
+          from_date = moment(from_date,"DD-MM-YYYY").format("YYYY-MM-DD HH:mm:ss")
+          q = q.whereRaw("created_at >= ?", from_date)
+        }
+        if (to_date) {
+          to_date = moment(to_date,"DD-MM-YYYY").format("YYYY-MM-DD HH:mm:ss")
+          q = q.whereRaw("created_at <= ?", to_date)
+        }
         if (email != '') {
             q.whereExists(function () {
                 this.from('users')
