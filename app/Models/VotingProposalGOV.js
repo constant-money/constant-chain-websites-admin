40527e95ece524e8245be64cbb9ffaa3b89f7b01
Duc Hung <hung.han@notabasement.com>
2019-03-05 18:07:08 +0700

    GOV proposal controller update

--- app/Models/VotingProposalGOV.js
+++ app/Models/VotingProposalGOV.js
@@ -10,6 +10,9 @@ class VotingProposalGOV extends Model {
     user() {
         return this.belongsTo('App/Models/User', 'user_id', 'id')
     }
+    votes(){
+      return this.hasMany('App/Models/VotingProposalGOVVote','id', 'voting_proposal_gov_id')
+    }
 }
 
 module.exports = VotingProposalGOV
