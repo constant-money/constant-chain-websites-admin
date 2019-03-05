8884a70ddf520ead302401049a5c8a61053cadfc
Duc Hung <hung.han@notabasement.com>
2019-03-05 18:06:55 +0700

    GOV & vote proposal query layer update

--- app/DAO/VotingProposalGOV.js
+++ app/DAO/VotingProposalGOV.js
@@ -2,6 +2,8 @@
 
 const VotingProposalGOVModel = use('VotingProposalGOVModel')
 
+const moment = require("moment")
+
 class VotingProposalGOV {
     async first(id) {
         return await VotingProposalGOVModel
@@ -11,11 +13,26 @@ class VotingProposalGOV {
             .where('id', id).first()
     }
 
-    async find({ email, page, perPage }) {
+    async find({ email, page, perPage, data, from_date, to_date, tx_id }) {
         let q = VotingProposalGOVModel
             .query()
             .with('user')
+            .withCount('votes as vote_count')
             .whereNull('deleted_at')
+        if (data) {
+          q = q.where("data", 'like',  '%' + data + '%')
+        }
+        if (tx_id) {
+          q = q.where("tx_id", tx_id)
+        }
+        if (from_date) {
+          from_date = moment(from_date,"DD-MM-YYYY").format("YYYY-MM-DD HH:mm:ss")
+          q = q.whereRaw("created_at >= ?", from_date)
+        }
+        if (to_date) {
+          to_date = moment(to_date,"DD-MM-YYYY").format("YYYY-MM-DD HH:mm:ss")
+          q = q.whereRaw("created_at <= ?", to_date)
+        }
         if (email != '') {
             q.whereExists(function () {
                 this.from('users')
--- app/DAO/VotingProposalGOVVote.js
+++ app/DAO/VotingProposalGOVVote.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const VotingProposalGOVVoteModel = use('VotingProposalGOVVoteModel')
+const moment = require("moment")
 
 class VotingProposalGOVVote {
     async first(id) {
@@ -13,12 +14,30 @@ class VotingProposalGOVVote {
             .where('id', id).first()
     }
 
-    async find({ votingProposalGOVId, email, page, perPage }) {
+    async find({ votingProposalGOVId, email, page, perPage, vote_amount_from, vote_amount_to, tx_id='', from_date='', to_date='' }) {
         let q = VotingProposalGOVVoteModel
             .query()
             .with('voter')
             .whereNull('deleted_at')
             .where('voting_proposal_gov_id', '=', votingProposalGOVId)
+        
+        if (vote_amount_from) {
+            q = q.whereRaw("vote_amount >= ?", vote_amount_from)
+        }
+        if (vote_amount_to) {
+            q = q.whereRaw("vote_amount <= ? ", vote_amount_to)
+        }
+        if (tx_id) {
+            q = q.where("tx_id", tx_id)
+        }
+        if (from_date) {
+            from_date = moment(from_date,"DD-MM-YYYY").format("YYYY-MM-DD HH:mm:ss")
+            q = q.whereRaw("created_at >= ?", from_date)
+        }
+        if (to_date) {
+            to_date = moment(to_date,"DD-MM-YYYY").format("YYYY-MM-DD HH:mm:ss")
+            q = q.whereRaw("created_at <= ?", to_date)
+        }
         if (email != '') {
             q.whereExists(function () {
                 this.from('users')
