2f4452da3cf0ff048255883ebba57cbf9a81492e
Apple <apple@Apples-MacBook-Pro.local>
2019-04-19 15:53:42 +0700

    Admin LTE

--- app/Controllers/Http/Admin/CandidateController.js
+++ app/Controllers/Http/Admin/CandidateController.js
@@ -4,77 +4,101 @@ const VotingBoardCandidateDAO = use('VotingBoardCandidateDAO')
 const VotingBoardVoteDAO = use('VotingBoardVoteDAO')
 
 class CandidateController {
-    /**
-     * Show a list of voting board candidates
-     * GET /votingboardcandidate
-     *
-     * @param {object} ctx
-     * @param {Request} ctx.request
-     * @param {Response} ctx.response
-     * @param {View} ctx.view
-     */
-    async index({ request, view }) {
-        const { email = '', dcb='', cmb = "", gov="", page = 1, perPage = 20 } = request.all()
-        const votingBoardCandidatesQ = await VotingBoardCandidateDAO.find(
-            { email: email, page: page, perPage: perPage, dcb, cmb, gov }
-        )
-        return view.render('admin/candidate/index', {
-            email: email,
-            dcb,
-            cmb,
-            gov,
-            page: votingBoardCandidatesQ.pages.page,
-            perPage: votingBoardCandidatesQ.pages.perPage,
-            lastPage: votingBoardCandidatesQ.pages.lastPage,
-            votingBoardCandidates: votingBoardCandidatesQ.rows,
-        })
+  /**
+   * Show a list of voting board candidates
+   * GET /votingboardcandidate
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, view }) {
+    const {
+      email = '',
+      candidateType = 'dcb',
+      candidateAddress = '',
+      page = 1,
+      perPage = 20
+    } = request.all()
+    try {
+      const votingBoardCandidatesQ = await VotingBoardCandidateDAO.find({
+        email,
+        page,
+        perPage,
+        candidateType,
+        candidateAddress
+      })
+      return view.render('admin/candidate/index', {
+        email,
+        candidateType,
+        candidateAddress,
+        page: votingBoardCandidatesQ.pages.page,
+        perPage: votingBoardCandidatesQ.pages.perPage,
+        lastPage: votingBoardCandidatesQ.pages.lastPage,
+        votingBoardCandidates: votingBoardCandidatesQ.rows
+      })
+    } catch (e) {
+      console.log(e.stack)
     }
-    /**
-     * Show a detail of voting board candidate
-     * GET /votingboardcandidate/:id
-     *
-     * @param {object} ctx
-     * @param {Request} ctx.request
-     * @param {Response} ctx.response
-     * @param {View} ctx.view
-     */
-    async show({ response, view, params }) {
-        const { id = 0 } = params
-        const votingBoardCandidate = await VotingBoardCandidateDAO.first(id)
-        if (votingBoardCandidate == undefined) {
-            return response.route('admin.candidate.index')
-        }
-        return view.render('admin/candidate/form', {
-            id: id,
-            votingBoardCandidate: votingBoardCandidate,
-        })
+  }
+  /**
+   * Show a detail of voting board candidate
+   * GET /votingboardcandidate/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ response, view, params }) {
+    const { id = 0 } = params
+    const votingBoardCandidate = await VotingBoardCandidateDAO.first(id)
+    if (votingBoardCandidate == undefined) {
+      return response.route('admin.candidate.index')
     }
-    /**
-     * Show a list of voting board candidates
-     * GET /votingboardcandidate
-     *
-     * @param {object} ctx
-     * @param {Request} ctx.request
-     * @param {Response} ctx.response
-     * @param {View} ctx.view
-     */
-    async voterIndex({ request, view, params }) {
-        const { id = 0 } = params
-        const { page = 1, perPage = 20, email="", tx_id="",board_type="" } = request.all()
-        const votingBoardVotesQ = await VotingBoardVoteDAO.find(
-            { votingBoardCandidateId: id, page: page, perPage: perPage, email, tx_id, board_type }
-        )
-        return view.render('admin/candidate/voter_index', {
-            id: id,
-            page: votingBoardVotesQ.pages.page,
-            perPage: votingBoardVotesQ.pages.perPage,
-            lastPage: votingBoardVotesQ.pages.lastPage,
-            votingBoardVotes: votingBoardVotesQ.rows,
-            email,
-            tx_id,
-            board_type,
-        })
-    }
+    return view.render('admin/candidate/form', {
+      id: id,
+      votingBoardCandidate: votingBoardCandidate
+    })
+  }
+  /**
+   * Show a list of voting board candidates
+   * GET /votingboardcandidate
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async voterIndex ({ request, view, params }) {
+    const { id = 0 } = params
+    const {
+      page = 1,
+      perPage = 20,
+      email = '',
+      tx_id = '',
+      board_type = ''
+    } = request.all()
+    const votingBoardVotesQ = await VotingBoardVoteDAO.find({
+      votingBoardCandidateId: id,
+      page: page,
+      perPage: perPage,
+      email,
+      tx_id,
+      board_type
+    })
+    return view.render('admin/candidate/voter_index', {
+      id: id,
+      page: votingBoardVotesQ.pages.page,
+      perPage: votingBoardVotesQ.pages.perPage,
+      lastPage: votingBoardVotesQ.pages.lastPage,
+      votingBoardVotes: votingBoardVotesQ.rows,
+      email,
+      tx_id,
+      board_type
+    })
+  }
 }
 
 module.exports = CandidateController
--- app/DAO/VotingBoardCandidate.js
+++ app/DAO/VotingBoardCandidate.js
@@ -3,44 +3,62 @@
 const VotingBoardCandidateModel = use('VotingBoardCandidateModel')
 
 class VotingBoardCandidate {
-    async first(id) {
-        return await VotingBoardCandidateModel
-            .query()
-            .with('user')
-            .whereNull('deleted_at')
-            .where('id', id).first()
-    }
+  async first (id) {
+    return await VotingBoardCandidateModel.query()
+      .with('user')
+      .whereNull('deleted_at')
+      .where('id', id)
+      .first()
+  }
 
-    async find({ email, page, perPage, dcb="", cmb="",gov="" }) {
-        let q = VotingBoardCandidateModel
-            .query()
-            .with('user')
-            .withCount('votes as vote_count')
-            .whereNull('deleted_at')
-        if (email != '') {
-            q.whereExists(function () {
-                this.from('users')
-                    .whereRaw('`users`.`id` = `voting_board_candidate`.`user_id`')
-                    .where('users.email', 'like', '%' + email + '%')
-            })
-        }
-        if (dcb) {
-            q.whereExists(function () {
-              q.where('dcb', dcb)
-            })
-        }
-        if (cmb) {
-            q.whereExists(function () {
-              q.where('dcb', cmb)
-            })
-        }
-        if (gov) {
-            q.whereExists(function () {
-              q.where('dcb', gov)
-            })
-        }
-        return await q.paginate(page, perPage)
+  async find ({ email, page, perPage, candidateType = '', candidateAddress }) {
+    let q = VotingBoardCandidateModel.query()
+      .with('user')
+      .withCount('votes as vote_count')
+      .whereNull('deleted_at')
+    if (email != '') {
+      q.whereExists(function () {
+        this.from('users')
+          .whereRaw('`users`.`id` = `voting_board_candidate`.`user_id`')
+          .where('users.email', 'like', '%' + email + '%')
+      })
     }
+    if (candidateType == 'dcb') {
+      if (candidateAddress) {
+        q.whereExists(function () {
+          q.where('dcb', 'like', `${candidateAddress}%`)
+        })
+      } else {
+        q.whereExists(function () {
+          q.where('dcb', '!=', '')
+        })
+      }
+    }
+    if (candidateType == 'cmb') {
+      console.log('123', candidateType, candidateAddress)
+      if (candidateAddress) {
+        q.whereExists(function () {
+          q.where('cmb', 'like', `${candidateAddress}%`)
+        })
+      } else {
+        q.whereExists(function () {
+          q.where('cmb', '!=', '')
+        })
+      }
+    }
+    if (candidateType == 'gov') {
+      if (candidateAddress) {
+        q.whereExists(function () {
+          q.where('gov', 'like', `${candidateAddress}%`)
+        })
+      } else {
+        q.whereExists(function () {
+          q.where('gov', '!=', '')
+        })
+      }
+    }
+    return await q.paginate(page, perPage)
+  }
 }
 
 module.exports = new VotingBoardCandidate()
